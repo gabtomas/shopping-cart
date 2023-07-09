93d8a2cf662e3900a52ae534b4682bcba12d4a33
gabtomas <46448735+gabtomas@users.noreply.github.com>
2023-07-09 15:07:48 +0200

    render store items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,16 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+
 export function Store() {
     return (
-        <div>
+        <>
             <h1>Store</h1>
-            <p>This is the Store page.</p>
-        </div>
+
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map((item) => (
+                    <Col>{JSON.stringify(item)}</Col>
+                ))}
+            </Row>
+        </>
     );
 }
